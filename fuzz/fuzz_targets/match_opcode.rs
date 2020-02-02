0ac9a8d4defa84a7b981f6643c8196d8c18fda40
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-02-02 14:05:28 -0500

    remove fuzz warnings
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- fuzz/fuzz_targets/match_opcode.rs
+++ fuzz/fuzz_targets/match_opcode.rs
@@ -5,8 +5,8 @@ use falcon_xtensa_lx6::match_opcode;
 
 fuzz_target!(|data: &[u8]| {
     if data.len() == 2 {
-        match_opcode((data[0], data[1], 0));
+        match_opcode((data[0], data[1], 0)).ok();
     } else if data.len() > 3 {
-        match_opcode((data[0], data[1], data[3]));
+        match_opcode((data[0], data[1], data[3])).ok();
     }
 });
