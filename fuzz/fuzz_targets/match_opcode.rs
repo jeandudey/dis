7a682fb696593861689506a6537226e59c74396c
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-02-02 15:58:39 -0500

    check match_opcode output
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- fuzz/fuzz_targets/match_opcode.rs
+++ fuzz/fuzz_targets/match_opcode.rs
@@ -4,9 +4,15 @@ use libfuzzer_sys::fuzz_target;
 use falcon_xtensa_lx6::match_opcode;
 
 fuzz_target!(|data: &[u8]| {
+    let mut bytes = (0, 0, 0);
     if data.len() == 2 {
-        match_opcode((data[0], data[1], 0)).ok();
+        bytes = (data[0], data[1], 0);
     } else if data.len() > 3 {
-        match_opcode((data[0], data[1], data[3])).ok();
+        bytes = (data[0], data[1], data[3]);
+    }
+
+    match match_opcode(bytes) {
+        Ok(_) => {},
+        Err(e) => println!("Failed: {}", e),
     }
 });
