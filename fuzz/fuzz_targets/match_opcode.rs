b899aa934051fa18811e49377c7663062c115a7b
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-02-02 13:05:36 -0500

    add cargo-fuzz to project
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

---
+++ fuzz/fuzz_targets/match_opcode.rs
@@ -0,0 +1,12 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+use falcon_xtensa_lx6::match_opcode;
+
+fuzz_target!(|data: &[u8]| {
+    if data.len() == 2 {
+        match_opcode((data[0], data[1], 0));
+    } else if data.len() > 3 {
+        match_opcode((data[0], data[1], data[3]));
+    }
+});
